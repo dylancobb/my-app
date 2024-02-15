430cfa9c0ffa8375a582794c0fbf3c0d8218a9ad
Dylan <122552492+dylancobb@users.noreply.github.com>
2024-02-15 13:09:55 +0000

    add basic interface (functions to be added)

---
+++ src/lib/Button.svelte
@@ -0,0 +1,7 @@
+<script lang="ts">
+    export let onClick = () => {};
+</script>
+
+<button on:click={onClick} class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+    <slot />
+</button>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,11 +1,23 @@
 <script>
-    let name = "Dylan";
+    import Button from "$lib/Button.svelte";
+    const addItem = () => {};
 </script>
 
-<h1 class="text-3xl font-bold underline">
-    Welcome to SvelteKit, {name}
-</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<div class="flex flex-col justify-center items-center">
+    <h1 class="text-4xl font-bold py-10">
+        Sveltekit + AWS Todo-List App
+    </h1>
+
+    <div class="w-full max-w-lg justify-center">
+        <form class="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
+            <div class="flex gap-1 mb-4">
+                <input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="username" type="text" placeholder="Shit to do">
+                <Button onClick={addItem}>Add</Button>
+            </div>
+        </form>
+    </div>
+
+</div>
 
 <style lang="postcss">
     :global(html) {
